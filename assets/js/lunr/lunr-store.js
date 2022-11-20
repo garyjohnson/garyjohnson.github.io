6475dca94013d475d265a535376e80acc9590a0f
garyjohnson <garyjohnson@users.noreply.github.com>
2022-11-20 05:03:35 +0000

    jekyll build from Action dc8b500d34ab2b0027203b593b085c8ff3426ed7

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "Bluetooth Build Button, Part 1: project planning",
+        "excerpt":"It’s time for a new project! I’ve been starting projects and enjoying not finishing them, but I’ve been feeling the itch to carry something small and focused all the way through design, documentation, and delivery. The Idea A few years ago I built a build button to run my tests...","categories": ["build button"],
+        "tags": [],
+        "url": "/projects/posts/new-project/",
+        "teaser": null
+      },{
+        "title": "Bluetooth Build Button, Part 2: the proof-of-concept",
+        "excerpt":"Well it didn’t take much to get the core idea (Part 1: new project) working. What you’re seeing here is the Adafruit Feather nrf52832 acting as a bluetooth keyboard, paired to my Macbook, and sending CTRL+ALT+F12 when I push the giant red button (mostly because it’s unlikely to be used...","categories": ["build button"],
+        "tags": [],
+        "url": "/projects/posts/button-mvp/",
+        "teaser": null
+      },{
+        "title": "Bluetooth Build Button, Part 3: the enclosure rough-ins",
+        "excerpt":"With the core idea now functional (Part 2: the proof-of-concept), it’s time to start working on a proper enclosure. For building models, I’ve been using Autodesk Fusion 360. It’s a powerful parametric modeling tool and has free non-commercial licensing. The license is only one year long, but can be renewed...","categories": ["build button"],
+        "tags": [],
+        "url": "/projects/posts/enclosure-rough-ins/",
+        "teaser": null
+      },{
+        "title": "Bluetooth Build Button, Part 4: v1.0 complete",
+        "excerpt":"With the rough-ins in place (Part 3: the enclosure rough-ins), we can build out the enclosure. Battery After seeing that the 2000mAh battery wasn’t going to fit, I moved to modeling the 500mAh one and tucked it under the Feather: Eventually I’d like everything to snap in without screws or...","categories": ["build button"],
+        "tags": [],
+        "url": "/projects/posts/build-button-v1/",
+        "teaser": null
+      },{
+        "title": "Bluetooth Build Button, Part 5: the software",
+        "excerpt":"Now that v1.0 is out of the way (Part 4: v1.0 complete), we can spend some time using it day-to-day and build incremental improvements. Goals I’ve made it through a week with the build button as a daily driver of launching tests, and it works great! The battery life is...","categories": ["build button"],
+        "tags": [],
+        "url": "/projects/posts/software/",
+        "teaser": null
+      },{
+        "title": "Bluetooth Build Button, Part 6: lights and a power switch",
+        "excerpt":"With a basic software implementation completed (Part 5: the software), we can spend some time using it day-to-day and build incremental improvements. Power switch I mentioned previously that I was opening the enclosure to turn the button on and off, so streamlining that behavior is my first target. A little...","categories": ["build button"],
+        "tags": [],
+        "url": "/projects/posts/lights-and-a-power-switch/",
+        "teaser": null
+      },{
+        "title": "Bluetooth Build Button, Part 7: crashes and animations",
+        "excerpt":"Previously, we added LEDs for feedback (Part 6: lights and a power switch), and ended up with a number of problems to fix. The most blocking was an issue where making changes to the firmware started causing inexplicable crashes. Crashes While working on animation code, I ran into an issue...","categories": ["build button"],
+        "tags": [],
+        "url": "/projects/posts/crashes-and-animations/",
+        "teaser": null
+      },{
+        "title": "Build button",
+        "excerpt":"A bluetooth button with USB charging for running tests Bluetooth Build Button, Part 7: crashes and animations Bluetooth Build Button, Part 6: lights and a power switch Bluetooth Build Button, Part 5: the software Bluetooth Build Button, Part 4: v1.0 complete Bluetooth Build Button, Part 3: the enclosure rough-ins Bluetooth...","categories": [],
+        "tags": [],
+        "url": "/projects/build-button/",
+        "teaser": null
+      }]
